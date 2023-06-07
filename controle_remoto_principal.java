2812cd107aaef6d5961d22cb160e2c24288abac8
jpfirmo <127751417+jpfirmo@users.noreply.github.com>
2023-06-07 19:19:31 -0300

    Add files via upload

---
+++ controle_remoto_principal.java
@@ -0,0 +1,20 @@
+package encapsulamento;
+
+public class controle_remoto_principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		ControleRemoto cr1 = new ControleRemoto();
+		cr1.ligar();
+		cr1.maisVolume();
+		cr1.abrirMenu();
+		cr1.fecharMenu();
+		
+		
+		
+		
+		
+	}
+
+}
